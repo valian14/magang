41c854b355e3e2bd65fc5f489323347fd0a250c8
RadenCakraD <mahardikawhite12@gmail.com>
2025-08-27 18:51:05 +0700

    aku tambahin beranda kohh ga jelas tapi wkwkwkwkwkwk

---
+++ Beranda-game.php
@@ -0,0 +1,277 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game Learn</title>
+    <link rel="stylesheet" href="Beranda-game.css">
+</head>
+<body>
+    <div class="background">
+        <div class="header">
+            <div class="judul">
+                <h1>Game Learn code</h1>
+            </div>
+            <div class="gambar">
+                <img src="" alt="">
+            </div>
+            <div class="Pengaturan"></div>
+        </div>
+    <main>
+        <div class="world-content">
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                    <h3 class="judul-world">LearnLand - Junior</h3>
+                <div class="level-world">
+                    <span class="minlevel-world">1</span>
+                    <span>/</span>
+                    <span class="maxlevel-world">200</span>
+                </div>
+                <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+            <div class="two-world">
+                <div class="world-small">
+                    <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                    <h3 class="judul-world">Mini Land 1</h3>
+                    <div class="level-world">
+                        <span class="minlevel-world">1</span>
+                        <span>/</span>
+                        <span class="maxlevel-world">50</span>
+                    </div>
+                    <div class="mainkan-world">
+                        <img src="./Assets-Beranda/Gambar/mainkan.png" width="40px">
+                    </div>
+                    <div class="keterangan-world">
+                        <p class="ket-umur">Umur 6+</p>
+                        <p class="ket-belajar">Dasar Coding</p>
+                    </div>
+                </div>
+                <div class="world-small">
+                    <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                    <h3 class="judul-world">Mini Land 2</h3>
+                    <div class="level-world">
+                        <span class="minlevel-world">1</span>
+                        <span>/</span>
+                        <span class="maxlevel-world">50</span>
+                    </div>
+                    <div class="mainkan-world">
+                        <img src="./Assets-Beranda/Gambar/mainkan.png" width="40px">
+                    </div>
+                    <div class="keterangan-world">
+                        <p class="ket-umur">Umur 6+</p>
+                        <p class="ket-belajar">Sintaks Dasar</p>
+                    </div>
+                </div>
+            </div>
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                <h3 class="judul-world">LearnLand - Junior</h3>
+                <div class="level-world">
+                    <span class="minlevel-world">1</span>
+                    <span>/</span>
+                    <span class="maxlevel-world">200</span>
+                </div>
+                <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                <h3 class="judul-world">LearnLand - Junior</h3>
+                <div class="level-world">
+                    <span class="minlevel-world">1</span>
+                    <span>/</span>
+                    <span class="maxlevel-world">200</span>
+                </div>
+                <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+            <div class="two-world">
+                <div class="world-small">
+                    <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                    <h3 class="judul-world">Mini Land 1</h3>
+                    <div class="level-world">
+                        <span class="minlevel-world">1</span>
+                        <span>/</span>
+                        <span class="maxlevel-world">50</span>
+                    </div>
+                    <div class="mainkan-world">
+                        <img src="./Assets-Beranda/Gambar/mainkan.png" width="40px">
+                    </div>
+                    <div class="keterangan-world">
+                        <p class="ket-umur">Umur 6+</p>
+                        <p class="ket-belajar">Dasar Coding</p>
+                    </div>
+                </div>
+                <div class="world-small">
+                    <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                    <h3 class="judul-world">Mini Land 2</h3>
+                    <div class="level-world">
+                        <span class="minlevel-world">1</span>
+                        <span>/</span>
+                        <span class="maxlevel-world">50</span>
+                    </div>
+                    <div class="mainkan-world">
+                        <img src="./Assets-Beranda/Gambar/mainkan.png" width="40px">
+                    </div>
+                    <div class="keterangan-world">
+                        <p class="ket-umur">Umur 6+</p>
+                        <p class="ket-belajar">Sintaks Dasar</p>
+                    </div>
+                </div>
+            </div>
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                <h3 class="judul-world">LearnLand - Junior</h3>
+                <div class="level-world">
+                    <span class="minlevel-world">1</span>
+                    <span>/</span>
+                    <span class="maxlevel-world">200</span>
+                </div>
+                <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                <h3 class="judul-world">LearnLand - Junior</h3>
+                <div class="level-world">
+                    <span class="minlevel-world">1</span>
+                    <span>/</span>
+                    <span class="maxlevel-world">200</span>
+                </div>
+                <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                    <h3 class="judul-world">LearnLand - Junior</h3>
+                    <div class="level-world">
+                        <span class="minlevel-world">1</span>
+                        <span>/</span>
+                        <span class="maxlevel-world">200</span>
+                    </div>
+                    <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                    <h3 class="judul-world">LearnLand - Junior</h3>
+                <div class="level-world">
+                    <span class="minlevel-world">1</span>
+                    <span>/</span>
+                    <span class="maxlevel-world">200</span>
+                </div>
+                <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                <h3 class="judul-world">LearnLand - Junior</h3>
+                <div class="level-world">
+                    <span class="minlevel-world">1</span>
+                    <span>/</span>
+                    <span class="maxlevel-world">200</span>
+                </div>
+                <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                    <h3 class="judul-world">LearnLand - Junior</h3>
+                    <div class="level-world">
+                    <span class="minlevel-world">1</span>
+                    <span>/</span>
+                    <span class="maxlevel-world">200</span>
+                </div>
+                <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+            <div class="world junior">
+                <img src="./Assets-Beranda/Gambar/Land.png" alt="" class="gambar-world">
+                <h3 class="judul-world">LearnLand - Junior</h3>
+                <div class="level-world">
+                    <span class="minlevel-world">1</span>
+                    <span>/</span>
+                    <span class="maxlevel-world">200</span>
+                </div>
+                <div class="mainkan-world">
+                    <img src="./Assets-Beranda/Gambar/mainkan.png" width="50px">
+                </div>
+                <div class="keterangan-world">
+                    <p class="ket-umur">Umur 6 - 12</p>
+                    <p class="ket-pulau">pulau yang sangat nyaman untuk belajar</p>
+                    <p class="ket-belajar">code dasar - sintaks - percabangan</p>
+                </div>
+            </div>
+        </div>
+    </main>
+        <div class="footer">
+            <div class="akun">
+                <img src="" alt="" class="profil-user">
+                <h4 class="nama-user"></h4>
+            </div>
+            <div class="mode">
+                A
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ World-LearnLand/Worldjunior-Stage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Junior-Stage</title>
+</head>
+<body>
+    <div class="background">
+
+    </div>
+    <div class="grid1"></div>
+</body>
+</html>
